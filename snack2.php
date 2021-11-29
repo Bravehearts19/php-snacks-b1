169a5fae86fcca65c62ec3308e3eb4f0cf6a861b
Bravehearts19 <leo18111998@gmail.com>
2021-11-29 17:10:39 +0100

    Snack 4 achieved and tries for snack 2

--- snack2.php
+++ snack2.php
@@ -1,5 +1,42 @@
 <?php 
-
+$posts = [
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +48,31 @@
         <title>Snack 2</title>
     </head>
     <body>
-        
+        <header></header>
+            <main>
+                <ul>
+                    <?php 
+                        for ($i=0; $i < count($posts); $i++) { 
+                            echo "
+                                <li>
+                                    <h3></h3>
+                                    <ul>
+                            ";
+                    ?>
+
+                    <?php  
+                        $arrayOfDates = array_keys($posts);
+                        for ($j=0; $j < count($arrayOfDates); $j++) { 
+                            echo $posts[$arrayOfDates][$j];
+                        }   echo "        
+
+                                    </ul>
+                                </li>
+                            "; 
+                        }
+                    ?>
+                </ul>
+            </main>
+            <footer></footer> 
     </body>
 </html>
--- snack4bonus.php
+++ snack4bonus.php
@@ -1,5 +1,31 @@
 <?php 
+    $longParagraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, ducimus voluptatem consectetur aliquam sequi saepe omnis ea nihil, voluptatibus cumque tempore necessitatibus praesentium ullam sunt ex? Molestias atque ab consequuntur!
+    Error excepturi aspernatur cupiditate provident modi dolorem et. Soluta magnam dicta doloribus sed, architecto, inventore, repellendus voluptas eveniet quis animi nobis atque nostrum cum tenetur et possimus velit dolorem deserunt!
+    Iusto ad eum cupiditate eligendi eos porro mollitia aspernatur dignissimos aliquam dolore. Quo voluptatibus harum fugit, accusamus vitae doloremque nostrum est magnam dolorem laboriosam nesciunt earum, veritatis sapiente ratione? Ullam.
+    Nisi, aut perspiciatis itaque accusamus dolorum esse unde porro repellendus animi quos commodi incidunt voluptates magni soluta beatae, delectus saepe modi inventore sapiente adipisci fugiat dicta officia nam. Voluptate, dicta.";
+    $definitiveParagraphArray = [];
+    
+    $paragraphArray = explode(".", $longParagraph);
+    for ($i=0; $i < count($paragraphArray); $i++) { 
+        if(!strrpos($paragraphArray[$i], '!') && !strrpos($paragraphArray[$i], '?')) {
+            array_push($definitiveParagraphArray, $paragraphArray[$i]);
+        } else {
+            $newParagraph = explode("!", $paragraphArray[$i]);
+            for ($j=0; $j < count($newParagraph); $j++)
+            if(!strrpos($newParagraph[$j], '?')) {
+                array_push($definitiveParagraphArray, $newParagraph[$j]); 
+            } else {
+                $newParagraph = explode("?", $newParagraph[$j]);
+                for ($k=0; $k < count($newParagraph); $k++) { 
+                    array_push($definitiveParagraphArray, $newParagraph[$j]); 
+                }
+            }
+        }
+    }
 
+    echo "<pre>";
+    var_dump($definitiveParagraphArray);
+    echo "</pre>";
 ?>
 
 <!DOCTYPE html>
